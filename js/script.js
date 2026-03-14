a0595098c290eed255f095cc853b47451b7e8bb8
ornelabaldini <ornelabaldin@gmail.com>
2026-03-14 12:52:03 -0300

    nuevo

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ const STOCK_PRODUCTOS = {
   "Lenguetazo Pinta Lengua (32 u)": 2,
   "Lenguetazo Tropical Punch (32 u)": 1,
   "Camión dispenser + caramelos rosa (1 unidad)": 1,
+  "Pistola + caramelos rosa (1 unidad)": 1,
 
   
 };
