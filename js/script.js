c846c665c20d9da60551c6b5227aed7ad08f7d28
golosinasaries <golosinasaries@gmail.com>
2026-05-14 21:13:06 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const minimoCompra = 50000; 
 const ENVIO_MDP = 9900;
-const ENVIO_GENERAL = 14900;
+const ENVIO_GENERAL = 15900;
 const ENVIO_LEJANO = 17900;
 const ENVIO_SANTACRUZ = 18900;
 const ENVIO_MIRAMAR= 0;
