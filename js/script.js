ba91e889e2fb8b65101c9e72a58672aa859dd4ac
golosinasaries <golosinasaries@gmail.com>
2026-06-15 20:27:01 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ let currentVariantes = null;
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const STOCK_PRODUCTOS = {
-  "Llaveros láser Capibara (1 u)": 5,
+  "Llaveros láser Capibara (1 u)": 2,
   "Chupetines Selección 384 g (24 u)": 1,
   "Pastillas D.R.F Sabor Anis (caja x 12 u)": 0,
   "Pastillas Alka sabor Cherry Mentol (caja x 12 u)": 0,
