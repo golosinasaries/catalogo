308d02d39d1b473fd87cad355266fa39b881b73e
golosinasaries <golosinasaries@gmail.com>
2026-06-15 20:56:59 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ let currentVariantes = null;
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const STOCK_PRODUCTOS = {
+  "Mogul Ladrillos Mundial 500 g": 1,
   "Llaveros láser Capibara (1 u)": 2,
   "Chupetines Selección 384 g (24 u)": 1,
   "Pastillas D.R.F Sabor Anis (caja x 12 u)": 0,
