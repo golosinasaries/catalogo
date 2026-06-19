8fd82dab82d8b7ba9c83bd012c109676b48f2524
golosinasaries <golosinasaries@gmail.com>
2026-06-19 20:41:41 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -1061,7 +1061,7 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 
     const envio = localStorage.getItem("codigoPostalCliente")
-      ? ((PROMO_ACTIVA === "envio" && total >= 150000) || total >= 300000
+      ? ((PROMO_ACTIVA === "envio" && total >= 80000) || total >= 300000
           ? 0
           : calcularCostoEnvio(localStorage.getItem("codigoPostalCliente")))
       : null;
@@ -1399,7 +1399,7 @@ document.getElementById("enviar-carrito")?.addEventListener("click", async (e) =
 
   // envío
   const envio =
-    (PROMO_ACTIVA === "envio" && total >= 150000) || total >= 300000
+    (PROMO_ACTIVA === "envio" && total >= 80000) || total >= 300000
       ? 0
       : calcularCostoEnvio(cp);
   msg += `\n📦 Total productos: ${totalProductos}`;
@@ -1495,7 +1495,7 @@ function actualizarAvisoEnvioGratis(total = 0, envioManualGratis = false) {
     }
 
   if (PROMO_ACTIVA === "envio") {
-    if (envioManualGratis || total >= 150000) {
+    if (envioManualGratis || total >= 80000) {
       aviso.innerHTML = "🎉 <strong>¡Tenés envío gratis!</strong>";
       aviso.style.display = "block";
 
@@ -1505,7 +1505,7 @@ function actualizarAvisoEnvioGratis(total = 0, envioManualGratis = false) {
         estadoEnvio.toastMostrado = true; 
       }
     } else {
-      const falta = 150000 - total;
+      const falta = 80000 - total;
       aviso.innerHTML = `Sumá <strong>$${falta.toLocaleString("es-AR")}</strong> y conseguí <b>envío gratis</b>`;
       aviso.style.display = "block";
       // reset
@@ -1957,7 +1957,7 @@ if (menuEnvio) {
   const total = calcularTotal();
 
   const costo =
-    (PROMO_ACTIVA === "envio" && total >= 150000) || total >= 300000
+    (PROMO_ACTIVA === "envio" && total >= 80000) || total >= 300000
       ? 0
       : calcularCostoEnvio(cp);
 
