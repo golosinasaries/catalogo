95d93481fc952a047527acfaef4c234fefada610
golosinasaries <golosinasaries@gmail.com>
2026-06-24 22:21:40 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const ENVIO_MIRAMAR= 0;
 const ENVIO_GRATIS = 0;
 const minimoRegalo = 60000;   
 const REGALO_NOMBRE = "Gomita Helado (30 u)";
-const PROMO_ACTIVA = "ninguna"; // opciones: "envio", "regalo", "ninguna"
+const PROMO_ACTIVA = "regalo"; // opciones: "envio", "regalo", "ninguna"
 
 let productos = [];
 let productoIndex = 0;
