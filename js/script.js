61886320dc8fa97895df0b4c4570470bd2d878bf
golosinasaries <golosinasaries@gmail.com>
2026-07-06 14:00:13 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const ENVIO_MIRAMAR= 0;
 const ENVIO_GRATIS = 0;
 const minimoRegalo = 60000;   
 const REGALO_NOMBRE = "Gomitas Monstruo (30u) 👾";
-const PROMO_ACTIVA = "regalo"; // opciones: "envio", "regalo", "ninguna"
+const PROMO_ACTIVA = "ninguna"; // opciones: "envio", "regalo", "ninguna"
 
 let productos = [];
 let productoIndex = 0;
