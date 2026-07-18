7dcbf1cfb1a4c17d0cec7cbd02f0d22e30a81c61
golosinasaries <golosinasaries@gmail.com>
2026-07-18 10:33:38 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const ENVIO_LEJANO = 14900;
 const ENVIO_SANTACRUZ = 15900;
 const ENVIO_MIRAMAR= 0;
 const ENVIO_GRATIS = 0;
-const minimoRegalo = 80000;   
-const REGALO_NOMBRE = "Gomitas Selección (30u) 🇦🇷 ";
-const PROMO_ACTIVA = "ninguna"; // opciones: "envio", "regalo", "ninguna"
+const minimoRegalo = 70000;   
+const REGALO_NOMBRE = "Gomitas Kuromy (30u) ";
+const PROMO_ACTIVA = "regalo"; // opciones: "envio", "regalo", "ninguna"
 
 let productos = [];
 let productoIndex = 0;
