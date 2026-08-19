12ec83e3994318541c66f5d7431e99a17fef7eac
golosinasaries <golosinasaries@gmail.com>
2026-08-18 21:12:16 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const STOCK_PRODUCTOS = {
   "Payaso con pastillitas (30u)": 1,
   "Chicles Tik Tok con tatoo (10u)": 1,
   "Mechas mágicas Lilo y Stitch (30u)": 1,
-  "Saca lenguas (30u)": 1,
+  "Saca lenguas (30u)": 0,
   "Pack Cañitos de queso 80g (6u)": 0,
   "Camiseta Pinball con pastillitas (30u)": 1,
   "Gomitas de Boca (30u)": 41,
