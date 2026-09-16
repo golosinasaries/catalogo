274780e5070756cdef888f2f323ef272d654c864
golosinasaries <golosinasaries@gmail.com>
2026-09-16 14:22:40 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -90,18 +90,6 @@ const globos = [
 
 const alcancias = [
 /*
-     {
-    nombre: "Alcancía Tigre Rojo (con 12 gelatinas en su interior)",
-    precio: 6900,
-    img: "img/tigrerojo.png"
-  },
-  
-     {
-    nombre: "Alcancía Tigre Amarillo (con 12 gelatinas en su interior)",
-    precio: 6900,
-    img: "img/alcanciaojoamarillo.png"
-  },
-  
 
   {
     nombre: "Alcancía Pingüino Negro (con 12 gelatinas en su interior)",
@@ -131,6 +119,17 @@ const alcancias = [
     nombre: "Alcancía Pollito Amarillo (con 12 gelatinas en su interior)",
     precio: 6900,
     img: "img/pollitoamarillo.png"
+  },
+    {
+    nombre: "Alcancía Tigre Rojo (con 12 gelatinas en su interior)",
+    precio: 6900,
+    img: "img/tigrerojo.png"
+  },
+  
+     {
+    nombre: "Alcancía Tigre Amarillo (con 12 gelatinas en su interior)",
+    precio: 6900,
+    img: "img/alcanciaojoamarillo.png"
   },
 /*
   {
