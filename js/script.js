d180b6e8121298986e6aa5a0d9fdbd1b257367c3
golosinasaries <golosinasaries@gmail.com>
2026-09-25 09:57:05 -0300

    Stock

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ const minimoCompra = 50000;
 const minimoRegalo = 50000;   
 const minimoEnvioGratis = 100000;
 
-const ENVIO_MDP = 6900;
+const ENVIO_MDP = 7900;
 const ENVIO_GENERAL = 12400;
 const ENVIO_LEJANO = 14400;
 const ENVIO_SANTACRUZ = 15400;
 const ENVIO_MIRAMAR= 0;
 const ENVIO_GRATIS = 0;
 
-const REGALO_NOMBRE = "1 Chupetines Pelotitas (50u)";
-const REGALO_IMAGEN = "img/pelotitas.jpg";
+const REGALO_NOMBRE = "1 Alcancía";
+const REGALO_IMAGEN = "img/alcanciaojoamarillo.png";
 
-const PROMOS_ACTIVAS = ["envio"]; 
+const PROMOS_ACTIVAS = ["regalo"]; 
 
 
 let productos = [];
